3c7125c4fae91ef9f9387e01235a964ba8229a4f
JsCodeDevlopment <jonatasilva118@gmail.com>
2025-07-20 16:46:29 -0300

    feat: add Portfolio component to showcase various sections including Hero, About, Projects, and Technologies

--- app/portfolio.tsx
+++ app/portfolio.tsx
@@ -3,15 +3,15 @@
 import { MouseFollower } from "@/components/mouse-follower";
 import { MarqueeSection } from "@/components/ui/marquee-section";
 import { technologies } from "@/constants/tecnologies.const";
-import { AboutSection } from "./components/about-section";
-import { ContactSection } from "./components/contact-section";
-import { EducationSection } from "./components/education-section";
-import { ExperienceSection } from "./components/experience-section";
-import { Header } from "./components/header";
-import { HeroSection } from "./components/hero-section";
-import { ProjectsSection } from "./components/projects-section";
-import { TechnologiesSection } from "./components/technologies-section";
-import { useTheme } from "./contexts/theme-context";
+import { AboutSection } from "../components/about-section";
+import { ContactSection } from "../components/contact-section";
+import { EducationSection } from "../components/education-section";
+import { ExperienceSection } from "../components/experience-section";
+import { Header } from "../components/header";
+import { HeroSection } from "../components/hero-section";
+import { ProjectsSection } from "../components/projects-section";
+import { TechnologiesSection } from "../components/technologies-section";
+import { useTheme } from "../contexts/theme-context";
 
 export default function Portfolio() {
   const { theme } = useTheme();
